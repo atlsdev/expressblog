814ea53f6d07a44042223634317966dd1654c4fe
Atlas <kontakt@streetracingdenmark.com>
2020-12-28 22:33:22 +0100

    chore: fix possible vulnerabilities

--- app.js
+++ app.js
@@ -8,24 +8,21 @@ var app = express();
 app.use(express.json());
 app.set("view engine", "ejs");
 
-app.get("/assets/:file", (req, res) => res.sendFile(`${__dirname}/assets/${req.params.file}`));
+app.get("/assets/:file", (req, res) => {
+	if(fs.existsSync(`${__dirname}/assets/${req.params.file}`)) {
+		res.sendFile(`${__dirname}/assets/${req.params.file}`);
+	} else {
+		res.sendStatus(404);
+	}
+});
 
 app.get("/", (req, res) => {
 	posts = fs.readdirSync(`${__dirname}/posts`).reverse();
-	var postsObj = posts.map(post => {
+	var postsObj = posts.length > 0 ? posts.map(post => {
 		obj = { name: post.replace(".txt", ""), content: md.render(fs.readFileSync(`${__dirname}/posts/${post}`, "utf8")) };
 		return obj;
-	});
+	}) : null;
 	res.render("index", { posts: postsObj });
-});
-
-app.get("/api/posts/", (req, res) => {
-	posts = fs.readdirSync(`${__dirname}/posts`).reverse();
-	var postsObj = posts.map(post => {
-		obj = { name: post.replace(".txt", ""), content: md.render(fs.readFileSync(`${__dirname}/posts/${post}`, "utf8")) };
-		return obj;
-	});
-	res.send({posts: postsObj});
 });
 
 app.listen(9999, () => console.log("Listening on 9999"));
